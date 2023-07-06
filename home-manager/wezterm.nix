ad87fca73e3305e598410806e40a78aa2063e785
A1029384756 <hayden.gray104@gmail.com>
2023-07-06 14:00:04 -0400

    mouse settings

--- home-manager/wezterm.nix
+++ home-manager/wezterm.nix
@@ -12,6 +12,7 @@
       config.color_scheme = 'Catppuccin Mocha'
       config.window_background_opacity = 0.9
       config.use_fancy_tab_bar = true
+      config.warn_about_missing_glyphs = false
       
       return config
     '';
