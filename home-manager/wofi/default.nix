24e3503975d53d1c3fc7679d62daef61355f8300
A1029384756 <hayden.gray104@gmail.com>
2023-07-10 01:24:07 -0400

    initial power operations (sleep needs fixed)

--- home-manager/wofi/default.nix
+++ home-manager/wofi/default.nix
@@ -1,9 +1,28 @@
 { config, pkgs, ... }: {
-  programs.waybar.enable = true;
-
   xdg.configFile.wofi = {
     source = ./conf;
     recursive = true;
     target = "wofi";
+  };
+
+  xdg.desktopEntries.shutdown = {
+    name = "Power Off";
+    icon = ./shutdown.svg;
+    exec = "shutdown 0";
+    terminal = false;
+  };
+
+  xdg.desktopEntries.suspend = {
+    name = "Sleep";
+    icon = ./suspend.svg;
+    exec = "systemctl hibernate";
+    terminal = false;
+  };
+
+  xdg.desktopEntries.restart = {
+    name = "Restart";
+    icon = ./reboot.svg;
+    exec = "reboot";
+    terminal = false;
   };
 }
