b9f154ed0be3685fc2ab7eee3561616bbdb8e0d0
A1029384756 <hayden.gray104@gmail.com>
2023-07-09 22:46:13 -0400

    fixed neovim desktop shortcut

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -1,14 +1,24 @@
 { config, pkgs, ... }: {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+  xdg.desktopEntries.nvim = {
+    name = "Neovim";
+    genericName = "Text Editor";
+    icon = ./nvim.svg;
+    exec = "wezterm start nvim %f";
+    terminal = false;
+    type = "Application";
+    categories = [ "Utility" "TextEditor" ];
   };
 
   xdg.configFile.nvim = {
     source = config.lib.file.mkOutOfStoreSymlink ./config;
     recursive = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
   };
 
   home.packages = with pkgs; [
