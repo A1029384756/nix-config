50d09d99afc2592d03097d96bf78655dae0d42ce
hgray <hgray@northwindstech.com>
2023-07-14 12:06:38 -0400

    template changes

--- templates/default.nix
+++ templates/default.nix
@@ -1,4 +1,7 @@
 {
-  rust.path = ./rust;
-  terraform_0_13_3.path = ./terraform_0_13_3
+  rust = {
+    path = ./rust;
+    description = "A starter Rust/Cargo project";
+  };
+  terraform_0_13_3.path = ./terraform_0_13_3;
 }
