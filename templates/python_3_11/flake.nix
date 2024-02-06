fea0c9cbb408434574ccd1738f5de6b85be195c5
A1029384756 <hayden.gray104@gmail.com>
2024-02-06 12:37:29 -0500

    foo

--- templates/python_3_11/flake.nix
+++ templates/python_3_11/flake.nix
@@ -2,24 +2,33 @@
   description = "Python 3.11 Devshell and Builds";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        pyEnv = pkgs.python311.withPackages(ps: [
+        pyEnv = pkgs.python311.withPackages (ps: [
         ]);
       in
       with pkgs;
       {
-        devShells.default = mkShell {
-          packages = [
-            pyEnv
-            pyright
-          ];
+        devShells = {
+          default = mkShell {
+            packages = [
+              pyEnv
+              pyright
+            ];
+          };
+
+          foobar = mkShell { };
         };
       }
     );
