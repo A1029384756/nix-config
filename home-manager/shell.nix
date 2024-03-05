df707d43027652b976815ab951dd67911c40a15c
A1029384756 <hayden.gray104@gmail.com>
2024-03-05 12:43:10 -0500

    updated fish config

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }: {
   programs.fish = {
     enable = true;
-    interactiveShellInit = "set fish_greeting";
+    interactiveShellInit = ''
+      set fish_greeting
+      fish_add_path ~/Odin
+      fish_add_path ~/ols
+    '';
     shellAliases = {
       ls = "eza --icons -F -H --group-directories-first --git -h $argv";
       cat = "bat --theme base16-256";
