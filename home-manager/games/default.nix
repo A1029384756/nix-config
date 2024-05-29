5073572208a93e425e6fc52125795bf60ff8c1d6
A1029384756 <hayden.gray104@gmail.com>
2024-05-29 16:45:08 -0400

    removed steam custom icon

--- home-manager/games/default.nix
+++ home-manager/games/default.nix
@@ -1,13 +1,4 @@
 { inputs, pkgs, ... }: {
-  xdg.desktopEntries.steam = {
-    name = "Steam";
-    genericName = "Games";
-    icon = ./steam.svg;
-    exec = "steam";
-    terminal = false;
-    type = "Application";
-  };
-
   home.file.".config/heroic-themes" = {
     source = inputs.heroic-theme;
     recursive = true;
