9f5080df8f655d8520b8539dc91f266a267fd38b
A1029384756 <hayden.gray104@gmail.com>
2024-06-05 23:41:42 -0400

    added spotify

--- home-manager/rev3/default.nix
+++ home-manager/rev3/default.nix
@@ -14,7 +14,17 @@
   home.packages = with pkgs; [
     brave
     mission-center
+    spotify
   ];
+
+  xdg.desktopEntries."com.spotify.Client" = {
+    name = "Spotify";
+    comment = "Spotify Client";
+    icon = "com.spotify.Client";
+    exec = "spotify";
+    categories = ["Audio"];
+    terminal = false;
+  };
 
   programs.home-manager.enable = true;
   home.stateVersion = "23.11";
