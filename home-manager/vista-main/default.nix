2ce0779ee0bc13e6742af57f6ca1cc3950e83db5
A1029384756 <hayden.gray104@gmail.com>
2024-10-29 16:33:34 -0400

    added vista server with valheim hosting

---
+++ home-manager/vista-main/default.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  imports = [
+    ../nvim.nix
+    ../shell.nix
+  ];
+
+  programs.home-manager.enable = true;
+  systemd.user.startServices = "sd-switch";
+  home.stateVersion = "23.05";
+}
