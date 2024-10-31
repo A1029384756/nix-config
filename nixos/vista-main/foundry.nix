fda2787734105760a00602089c9caf34837f1dc7
A1029384756 <hayden.gray104@gmail.com>
2024-10-31 11:13:44 -0400

    moved foundry configuration into a function

--- nixos/vista-main/foundry.nix
+++ nixos/vista-main/foundry.nix
@@ -1,76 +1,63 @@
-{ inputs, ... }: {
-  containers.theros = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.11";
+{ inputs, ... }:
+let
+  foundry_instance = { name, version, hostAddress, localAddress }:
+    let
+      host = "cstring.dev";
+    in
+    {
+      containers.${name} = {
+        autoStart = true;
+        privateNetwork = true;
+        inherit hostAddress;
+        inherit localAddress;
 
-    bindMounts.foundrydata = {
-      mountPoint = "/var/lib/foundryvtt";
-      isReadOnly = false;
-      hostPath = "/home/haydengray/theros";
-    };
+        bindMounts.foundrydata = {
+          mountPoint = /var/lib/foundryvtt;
+          isReadOnly = false;
+          hostPath = /home/haydengray/${name};
+        };
 
-    config = with inputs; {
-      imports = [ foundryvtt.nixosModules.foundryvtt ];
-      services.foundryvtt = {
-        enable = true;
-        hostName = "cstring.dev";
-        proxySSL = true;
-        proxyPort = 443;
-        package = foundryvtt.packages.x86_64-linux.foundryvtt.overrideAttrs {
-          version = "12.0.0+331";
+        config = with inputs; {
+          imports = [ foundryvtt.nixosModules.foundryvtt ];
+          services.foundryvtt = {
+            enable = true;
+            hostName = host;
+            proxySSL = true;
+            proxyPort = 443;
+            package = foundryvtt.packages.x86_64-linux.foundryvtt.overrideAttrs {
+              inherit version;
+            };
+          };
+
+          networking.firewall = {
+            enable = true;
+            allowedTCPPorts = [ 30000 ];
+          };
         };
       };
 
-      networking.firewall = {
-        enable = true;
-        allowedTCPPorts = [ 30000 ];
-      };
+      services.caddy.virtualHosts."${name}.${host}".extraConfig = ''
+        reverse_proxy ${localAddress}:30000
+      '';
     };
-  };
+in
+{
+  imports = [
+    (foundry_instance ({
+      name = "theros";
+      version = "12.0.0+331";
+      hostAddress = "192.168.100.10";
+      localAddress = "192.168.100.11";
+    }))
+    (foundry_instance ({
+      name = "ewington";
+      version = "12.0.0+331";
+      hostAddress = "192.168.100.12";
+      localAddress = "192.168.100.13";
+    }))
+  ];
 
-  containers.ewington = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.100.12";
-    localAddress = "192.168.100.13";
-
-    bindMounts.foundrydata = {
-      mountPoint = "/var/lib/foundryvtt";
-      isReadOnly = false;
-      hostPath = "/home/haydengray/ewington";
-    };
-
-    config = with inputs; {
-      imports = [ foundryvtt.nixosModules.foundryvtt ];
-      services.foundryvtt = {
-        enable = true;
-        hostName = "cstring.dev";
-        proxySSL = true;
-        proxyPort = 443;
-        package = foundryvtt.packages.x86_64-linux.foundryvtt.overrideAttrs {
-          version = "12.0.0+331";
-        };
-      };
-
-      networking.firewall = {
-        enable = true;
-        allowedTCPPorts = [ 30000 ];
-      };
-    };
-  };
-
-  services.caddy = {
-    enable = true;
-    virtualHosts."theros.cstring.dev".extraConfig = ''
-      reverse_proxy 192.168.100.11:30000
-    '';
-    virtualHosts."ewington.cstring.dev".extraConfig = ''
-      reverse_proxy 192.168.100.13:30000
-    '';
-  };
-
+  services.caddy.enable = true;
   networking.nat = {
     enable = true;
     internalInterfaces = [ "ve-+" ];
