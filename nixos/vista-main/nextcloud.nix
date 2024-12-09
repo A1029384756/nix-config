90eea28a9fe1aa0cb1f63fa7fa02d07f9261fe39
A1029384756 <hayden.gray104@gmail.com>
2024-12-09 15:04:25 -0500

    fixed multifile downloads

--- nixos/vista-main/nextcloud.nix
+++ nixos/vista-main/nextcloud.nix
@@ -1,29 +1,94 @@
 { config, pkgs, ... }:
+let
+  hostname = "cloud.cstring.dev";
+in
 {
-  imports = [
-    "${fetchTarball {
-    url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
-    sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
-  ];
-
   age.secrets.nextcloud_admin = {
     file = ../../secrets/nextcloud_admin.age;
     owner = "nextcloud";
     group = "nextcloud";
   };
 
+  services.nginx.enable = false;
   services.nextcloud = {
     enable = true;
     https = true;
     package = pkgs.nextcloud30;
-    hostName = "cloud.cstring.dev";
+    hostName = hostname;
     config.adminpassFile = config.age.secrets.nextcloud_admin.path;
 
     extraAppsEnable = true;
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
     };
+  };
 
-    webserver = "caddy";
+  services.phpfpm.pools.nextcloud.settings = {
+    "listen.owner" = config.services.caddy.user;
+    "listen.group" = config.services.caddy.group;
+  };
+
+  services.caddy.virtualHosts."https://${hostname}" = {
+    extraConfig = ''
+          encode zstd gzip
+
+          root * ${config.services.nginx.virtualHosts.${hostname}.root}
+
+          redir /.well-known/carddav /remote.php/dav 301
+          redir /.well-known/caldav /remote.php/dav 301
+          redir /.well-known/* /index.php{uri} 301
+          redir /remote/* /remote.php{uri} 301
+
+          header {
+            Strict-Transport-Security max-age=31536000
+            Permissions-Policy interest-cohort=()
+            X-Content-Type-Options nosniff
+            X-Frame-Options SAMEORIGIN
+            Referrer-Policy no-referrer
+            X-XSS-Protection " 1;
+      mode = block "
+            X-Permitted-Cross-Domain-Policies none
+            X-Robots-Tag "
+        noindex, nofollow"
+            -X-Powered-By
+          }
+
+          php_fastcgi unix/${config.services.phpfpm.pools.nextcloud.socket} {
+            root ${config.services.nginx.virtualHosts.${hostname}.root}
+            env front_controller_active true
+            env modHeadersAvailable true
+          }
+
+          @forbidden {
+            path /build/* /tests/* /config/* /lib/* /3rdparty/* /templates/* /data/*
+            path /.* /autotest* /occ* /issue* /indie* /db_* /console*
+            not path /.well-known/*
+          }
+          error @forbidden 404
+
+          @notlegacy {
+              path *.php *.php/
+              not path /index* /remote* /public* /cron* /core/ajax/update* /status* /ocs/v1* /ocs/v2* /updater* /ocs-provider/* */richdocumentscode/proxy*
+          }
+          rewrite @notlegacy /index.php{uri}
+
+          @immutable {
+            path *.css *.js *.mjs *.svg *.gif *.png *.jpg *.ico *.wasm *.tflite
+            query v=*
+          }
+          header @immutable Cache-Control "max-age=15778463, immutable"
+
+          @static {
+            path *.css *.js *.mjs *.svg *.gif *.png *.jpg *.ico *.wasm *.tflite
+            not query v=*
+          }
+          header @static Cache-Control "max-age=15778463"
+
+          @woff2 path *.woff2
+          header @woff2 Cache-Control "max-age=604800"
+
+          file_server
+    '';
   };
 }
+
