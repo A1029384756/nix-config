10174455b7c09848ee22ae8112aa79ac99491d2e
A1029384756 <hayden.gray104@gmail.com>
2024-12-24 19:33:44 -0500

    added wedding and blog sites

---
+++ nixos/vista-main/wedding_site.nix
@@ -0,0 +1,50 @@
+{ inputs, pkgs, ... }:
+let
+  site = inputs.wedding.packages.x86_64-linux.hayden_anna_wedding;
+  basepath = "/wedding";
+in
+{
+  services.caddy.virtualHosts."cstring.dev".extraConfig = ''
+    handle ${basepath}* {
+      reverse_proxy 192.168.101.11:8000
+    }
+  '';
+
+  containers.wedding = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.101.10";
+    localAddress = "192.168.101.11";
+
+    config = {
+      nixpkgs.config.allowUnfree = true;
+
+      environment = {
+        etc."wedding/site".source = inputs.wedding;
+      };
+
+      systemd.services.wedding = {
+        enable = true;
+        environment.BASE_PATH = "/wedding";
+        script = ''
+          cp -r /etc/wedding/site/* /root
+          cd /root
+          ${pkgs.lib.getExe pkgs.deno} task preview
+        '';
+        wantedBy = [ "multi-user.target" ];
+      };
+
+      services.mongodb = {
+        enable = true;
+        package = pkgs.mongodb-ce;
+      };
+
+      networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [ 8000 ];
+      };
+
+      system.stateVersion = "24.11";
+    };
+  };
+}
