a5d6832f571cedc82d7bc7a45211faae3eb29c06
A1029384756 <hayden.gray104@gmail.com>
2025-05-06 15:44:28 -0400

    updated to add minecraft server and dotfiles

--- nixos/vista-main/minecraft.nix
+++ nixos/vista-main/minecraft.nix
@@ -7,6 +7,7 @@
     eula = true;
     openFirewall = true;
 
+    # saved in /srv/minecraft/frennz
     servers.frennz = {
       enable = true;
       enableReload = true;
@@ -15,10 +16,11 @@
         motd = "NOT sponsored by nezcorp";
         difficulty = 2;
         gamemode = "survival";
+        level-name = "ZeroFrennzWorld";
       };
 
       jvmOpts = "-Xms4092M -Xmx4092M";
-      package = pkgs.papermcServers.papermc-1_21_4;
+      package = pkgs.papermcServers.papermc-1_21_5;
 
       symlinks = {
         "server-icon.png" = ./minecraft_assets/server-icon.png;
