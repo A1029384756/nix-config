d3f43d1d8d5a00ca21d3846dc516ccc737226c8c
A1029384756 <hayden.gray104@gmail.com>
2026-01-06 22:52:41 -0500

    [vista] fixed cockpit (will remove it later)

--- nixos/vista/cockpit.nix
+++ nixos/vista/cockpit.nix
@@ -2,6 +2,9 @@
   services.cockpit = {
     enable = true;
     openFirewall = true;
+		allowed-origins = [
+			"https://192.168.1.51:9090"
+		];
     settings = {
       WebService = {
         AllowUnencrypted = true;
