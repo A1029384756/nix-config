065dc01773556bb4d26d95f0dfb6e3319c836e8b
A1029384756 <hayden.gray104@gmail.com>
2026-01-07 20:18:39 -0500

    [blog] spacing

--- nixos/vista/blog.nix
+++ nixos/vista/blog.nix
@@ -1,5 +1,4 @@
-{ inputs, pkgs, ... }:
-{
+{ inputs, pkgs, ... }: {
   services.caddy.virtualHosts."cstring.dev".extraConfig = ''
 		root * ${inputs.blog.packages.${pkgs.system}.blog}
 		file_server
