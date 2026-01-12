c2574f5b574cbd0c8383a52e8838e4581465c035
A1029384756 <hayden.gray104@gmail.com>
2026-01-11 23:15:49 -0500

    [vista] cockpit now accessible on tailnet

--- nixos/vista/cockpit.nix
+++ nixos/vista/cockpit.nix
@@ -1,18 +1,23 @@
 {
-  services.cockpit = {
-    enable = true;
-    openFirewall = true;
-		allowed-origins = [
-			"https://192.168.1.51:9090"
-			"https://cockpit.vista.tailnet.cstring.dev"
+	services = {
+		cockpit = {
+			enable = true;
+			allowed-origins = [
+				"https://cockpit.vista.tailnet.cstring.dev"
+			];
+		};
+		caddy.virtualHosts."cockpit.vista.tailnet.cstring.dev".extraConfig = ''
+			tls {
+				dns cloudflare {env.CF_API_TOKEN}
+			}
+			reverse_proxy localhost:9090
+		'';
+		headscale.settings.dns.extra_records = [
+			{
+				name = "cockpit.vista.tailnet.cstring.dev";
+				type = "A";
+				value = "100.64.0.5";
+			}
 		];
-    settings = {
-      WebService = {
-        AllowUnencrypted = true;
-      };
-    };
-  };
-	services.caddy.virtualHosts."cockpit.vista.tailnet.cstring.dev".extraConfig = ''
-		reverse_proxy localhost:9090
-	'';
+	};
 }
