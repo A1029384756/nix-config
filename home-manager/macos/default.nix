da0ccfad8eac19e1d57abb3ce2e680e9a15f4791
A1029384756 <hayden.gray104@gmail.com>
2026-01-17 23:49:29 -0500

    [flake] tree-shaking

--- home-manager/macos/default.nix
+++ home-manager/macos/default.nix
@@ -9,9 +9,7 @@
   ];
 
   config = {
-    home.file.".config/ghostty/config".source = ./ghosty_config;
     isWork = true;
-
     fonts.fontconfig.enable = true;
     programs.home-manager.enable = true;
     home.stateVersion = "23.11";
